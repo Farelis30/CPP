9f247e0f1413d5f33ee50be40a11d6110709f8b1
Farelis30 <farelyudistira01@gmail.com>
2023-10-11 12:56:08 +0700

    add folder variables

---
+++ variable/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int berisi integer(semua angka) tanpa desimal, seperti 111 atau -111
+    int a = 123;
+
+    // double berisi pecahan desimal dengan titik ambang, seperti 123,123 atau -123,123
+    double b = 123.123;
+
+    // char berisi satu character, seperti 'A' atau 'B'. nilai char harus berada di tengah single quote ('')
+    char c = 'C';
+
+    // string berisi kalimat, Seperti "Halo". nilai string harus berada di tengah double quote("")
+    string d = "Hallo World";
+
+    // bool berisi boolean yang hanya menyimpan kondisi benar atau salah (true or false)
+    bool e = true;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+    cout << "d = " << d << endl;
+    cout << "e = " << e << endl;
+   
+}
