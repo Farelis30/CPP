5e7e436740d393f84093eb421bb31b1426533edd
Farelis30 <farelyudistira01@gmail.com>
2023-10-11 19:25:56 +0700

    add comment

---
+++ comment/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // {"Dibawah baris ini adalah kode yang tidak akan dieksekusi"}
+    // single line comment (komen satu baris)
+
+    /*
+        Ini adalah multi-line comment
+        (Komen multi baris)
+    */
+
+    // {"Dibawah baris ini adalah kode yang akan dieksekusi"}
+    cout << "Ini adalah kode yang akan dieksekusi" << endl;
+}
